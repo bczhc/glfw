219c8e391caedac82f54628cfe63db1dcf9ee480
Recursive G <g.recursive@gmail.com>
2021-10-05 22:30:52 -0500

    Properly discard filtered events by XFilterEvent()
    According to https://www.x.org/releases/current/doc/man/man3/XFilterEvent.3.xhtml,
    an event should be discarded if XFilterEvent returns `True`. Fix #1794.

--- src/x11_window.c
+++ src/x11_window.c
@@ -1359,6 +1359,10 @@ static void processEvent(XEvent *event)
         keycode = event->xkey.keycode;
 
     filtered = XFilterEvent(event, None);
+    if (filtered)
+    {
+        return;
+    }
 
     if (_glfw.x11.randr.available)
     {
